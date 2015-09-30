0b54b6a4dc217c23f2f4b24b3bb42415d7b9f781
emdemagalhaes <emdemagalhaes@gmail.com>
2015-09-30 10:42:33 -0300

    criado um for

--- src/br/univel/teste/Teste.java
+++ src/br/univel/teste/Teste.java
@@ -2,6 +2,9 @@ package br.univel.teste;
 
 public class Teste {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello world");
+		}
+
 	}
 }
